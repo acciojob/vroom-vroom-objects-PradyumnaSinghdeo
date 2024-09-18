55939ba220e179442caf9cd4b9b3f41cd5c47b6f
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-09-18 12:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-// Complete the js code
-function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
+
+Car.prototype.getMakeModel = function() {
+  return this.make + " " + this.model;
+};
+
+
+function SportsCar(make, model, topSpeed) {
+  // Inherit make and model from Car
+  Car.call(this, make, model); // Call the Car constructor with this
+  this.topSpeed = topSpeed;    // Add topSpeed specific to SportsCar
+}
+
+
+SportsCar.prototype = Object.create(Car.prototype);
+
+
+SportsCar.prototype.constructor = SportsCar;
+
+
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
